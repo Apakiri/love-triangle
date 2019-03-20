a290ef6692e798a88e9401a2f4707d4e390f4c64
Kirill Apanasiuk <Micapka@yandex.ru>
2019-03-20 23:16:31 +0300

    fix:

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  
+  var numb_of_love_triangles = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    var next_index = preferences[i] - 1;
+    if (next_index == i) continue;
+    var Nnext_index = preferences[next_index] - 1;
+    if (Nnext_index == next_index) continue;
+    var NNnext_index = preferences[Nnext_index] - 1;
+    if (NNnext_index == Nnext_index) continue;
+    if (NNnext_index == i) {
+      numb_of_love_triangles++;
+    }
+  }
+  return numb_of_love_triangles / 3;
+}
